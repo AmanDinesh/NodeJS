14eb6efb457161cc8dd1f886c30afa2949922d3e
Aman Prajapat <amandineshprajapat@gmail.com>
2019-03-30 18:11:41 +0530

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '168248184055772',
+        clientSecret: 'abd6ab674d4985cc15f395edcce01a97'
+    }
 }
